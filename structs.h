585ee17366033e3686aa8aa3834b61b8c039a51d
Dave <davewilliams1978@yahoo.com>
2014-06-10 19:44:52 -0400

    I added intro comments to both structs-0.c and structs.h.

--- structs.h
+++ structs.h
@@ -1,4 +1,39 @@
+/**
+ *
+ *
+ * structs.h
+ * 
+ * Header file for structs-0.c
+ * 
+ * This program is a modified and augmented version of the Professor Malan's 
+ * example from CS50, except with a much larger data structure.  This program
+ * will generate a basic CSV file, which will be used in a future program to
+ * experiment with filling a data structure from information from a CSV file.
+ *
+ * The information intended to be entered when this program is ran was obtained
+ * from Wikipedia searches, and pertain to 10 historic major volcanic eruptions:
+ * 
+ * 1. Toba Catastrophe
+ * 2. Huckleberry Ridge Tuff - Yellowstone
+ * 3. Fish Canyon Tuff - La Garita Caldera
+ * 4. Pacana Eruption
+ * 5. Oruanui Eruption - Lake Taupo
+ * 6. Lava Creek Tuff - Yellowstone
+ * 7. Cerro Galan Eruption
+ * 8. Hatepe Eruptuion of 180 C.E. - Lake Taupo
+ * 9. Mount Tambora Eruption of 1815 C.E.
+ * 10. Krakatoa Eruption of 1883 C.E.
+ * 
+ *
+**/
 #include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define ERUPTIONS 10
+
 
 typedef struct eruption
 {
